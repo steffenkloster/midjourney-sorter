ac050cc1178c27e8f51f531e239c31a84ae1b1fe
Steffen Andreas Kloster <steffenakloster@gmail.com>
2023-11-10 16:41:57 +0100

    new command

---
+++ commands/archive.js
@@ -0,0 +1,58 @@
+const { SlashCommandBuilder, ChannelType } = require("discord.js");
+
+module.exports = {
+  data: new SlashCommandBuilder()
+    .setName("archive")
+    .setDescription("Archives a category")
+    .addStringOption((option) =>
+      option
+        .setName("category")
+        .setDescription("Name of the category")
+        .setRequired(false)
+    ),
+  async execute(interaction) {
+    const getCategoryName = (interaction) => {
+      if (interaction.options.getString("category") == null) {
+        const thisChannel = interaction.guild.channels.cache.find(
+          (channel) => channel.id == interaction.channelId
+        );
+        const parentChannel = interaction.guild.channels.cache.find(
+          (channel) => channel.id == thisChannel.parentId
+        );
+
+        if (parentChannel.name !== "Categories") {
+          return false;
+        }
+
+        return thisChannel.name;
+      } else {
+        return interaction.options
+          .getString("category")
+          .toLowerCase()
+          .replace(/ /g, "-");
+      }
+    };
+
+    const categoryName = getCategoryName(interaction);
+
+    const categoryChannel = interaction.guild.channels.cache.find(
+      (channel) =>
+        channel.parentId == "1121448960339497041" &&
+        channel.name == categoryName
+    );
+    const upscaledChannel = interaction.guild.channels.cache.find(
+      (channel) =>
+        channel.parentId == "1121449008519454770" &&
+        channel.name.endsWith(`${categoryName}-upscaled`)
+    );
+
+    if (categoryChannel || upscaledChannel) {
+      if (categoryChannel) categoryChannel.delete();
+      if (upscaledChannel) upscaledChannel.setParent("1159821433829064744");
+
+      interaction.reply("Archived!");
+    } else {
+      interaction.reply("Couldn't find category");
+    }
+  },
+};
